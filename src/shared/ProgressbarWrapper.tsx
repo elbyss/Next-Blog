4ad6c50bd7444c74f2aa794bca6a7635ab074c78
Elbyss <elysiabyss@gmail.com>
2024-08-08 16:20:53 +0900

    design: change codeblock font-family

--- src/shared/ProgressbarWrapper.tsx
+++ src/shared/ProgressbarWrapper.tsx
@@ -2,7 +2,7 @@
 import ProgressBar from 'react-scroll-progress-bar';
 export default function ProgressbarWrapper() {
   return (
-    <div className='absolute inset-0'>
+    <div className='absolute left-0'>
       <ProgressBar bgcolor='#a580ff' height='4' duration='0' />
     </div>
   );
