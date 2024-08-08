ec24f54757d387a00606bf8367a7f63d674e007e
Elbyss <elysiabyss@gmail.com>
2024-08-08 14:33:13 +0900

    feat: Add progressbar

--- src/shared/CategoryLayout.tsx
+++ src/shared/CategoryLayout.tsx
@@ -3,20 +3,30 @@ import { meta } from '../utils/meta';
 import Image from 'next/image';
 import { ReactNode } from 'react';
 import Link from 'next/link';
+import ProgressbarWrapper from './ProgressbarWrapper';
 
 export default function CategoryLayout({ children }: { children: ReactNode }) {
   return (
-    <div className={`w-full flex flex-col items-center p-3`}>
-      <header className='w-full max-w-3xl flex justify-between items-center my-4'>
-        <Link href='/'>
-          <div className='flex flex-row items-center gap-4'>
-            <Image src='/logo.svg' alt='logo' width={100} height={48} className='object-cover rounded-3xl h-12'></Image>
-            <span className='font-bold text-2xl'>{meta.title}</span>
-          </div>
-        </Link>
-        <Navbar />
-      </header>
-      <main className='w-full max-w-3xl'>{children}</main>
-    </div>
+    <>
+      <ProgressbarWrapper />
+      <div className={`w-full flex flex-col items-center p-3`}>
+        <header className='w-full max-w-3xl flex justify-between items-center my-4'>
+          <Link href='/'>
+            <div className='flex flex-row items-center gap-4'>
+              <Image
+                src='/logo.svg'
+                alt='logo'
+                width={100}
+                height={48}
+                className='object-cover rounded-3xl h-12'
+              ></Image>
+              <span className='font-bold text-2xl'>{meta.title}</span>
+            </div>
+          </Link>
+          <Navbar />
+        </header>
+        <main className='w-full max-w-3xl'>{children}</main>
+      </div>
+    </>
   );
 }
---
+++ src/shared/ProgressbarWrapper.tsx
@@ -0,0 +1,9 @@
+'use client';
+import ProgressBar from 'react-scroll-progress-bar';
+export default function ProgressbarWrapper() {
+  return (
+    <>
+      <ProgressBar bgcolor='#a580ff' height='4' duration='0.1' />
+    </>
+  );
+}
