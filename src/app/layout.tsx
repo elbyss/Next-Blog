f53f13710dd4ddd76867971caa24ac6a971d4466
Elbyss <elysiabyss@gmail.com>
2024-08-10 22:42:50 +0900

    chore: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: meta.title,
   description: meta.description,
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
