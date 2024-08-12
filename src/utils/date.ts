8a32af66b80a61db98108af78576be4bc5a744a7
Elbyss <elysiabyss@gmail.com>
2024-08-13 00:53:00 +0900

    fix: timezone issue

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,12 @@
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const KOREA_TIMEZONE = 'Asia/Seoul';
 
 export function formatDate(date: Date | string): string {
-  return dayjs(date).format('YYYY년 MM월 DD일 HH:mm');
+  return dayjs(date).tz(KOREA_TIMEZONE).format('YYYY년 MM월 DD일 HH:mm');
 }
