2a91983173901383b4b94fce6a4c7c6898f74ae5
Elbyss <elysiabyss@gmail.com>
2024-08-13 12:38:38 +0900

    chore: change sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   siteUrl: 'https://elbyss.vercel.app',
   generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.8,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
 };
