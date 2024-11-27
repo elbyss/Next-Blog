4196eb814523d214575e5d97ca39ba3f8c4c0aeb
Elbyss <elysiabyss@gmail.com>
2024-11-28 01:21:26 +0900

    Chore: remove unused files

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteUrl: 'https://elbyss.xyz',
-  generateRobotsTxt: true,
-  changefreq: 'daily',
-  priority: 0.8,
-  sitemapSize: 7000,
-  generateRobotsTxt: true,
-  exclude: [],
-};
