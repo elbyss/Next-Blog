f2889dcbf412837fee05aa22fba818079b5501fc
Elbyss <elysiabyss@gmail.com>
2024-12-17 17:15:21 +0900

    SEO: sitemap, robots 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const postSitemaps: MetadataRoute.Sitemap = allPosts.map((post) => ({
     url: `https://elbyss.xyz/post/${post._raw.flattenedPath}`,
     lastModified: new Date(post.date),
-    changeFrequency: 'daily',
+    changeFrequency: 'always',
   }));
 
   return [
